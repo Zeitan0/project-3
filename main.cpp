b3fadb3715853887aafa4302a2ec5b259d3b6835
Zeitan0 <122420083+Zeitan0@users.noreply.github.com>
2024-12-03 23:22:50 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,133 +5,65 @@
 #include <vector>
 #include <queue>
 #include <sstream>
+#include <chrono>
 #include "pugixml-master/src/pugixml.hpp"
-
+#include "Trie.h"
+#include "hashTable.h"
 using namespace std;
 
-class TrieNode{
-public:
-    unordered_map<char, TrieNode*> children;
-    bool end_of_word;
-
-    TrieNode()
-    {
-        end_of_word=false;
-    }
-};
-
-class Trie
+bool parseWikipediaDumpIntoTrie(const string& filename, Trie& trie)
 {
-    TrieNode* root;
-    unordered_map<string,bool> cache;
-
-public:
-    Trie() {
-        root = new TrieNode();
+    //reference to pugixml library
+    pugi::xml_document doc;
+    pugi::xml_parse_result result= doc.load_file(filename.c_str());
+    if (!result)
+    {
+        cout << "XML file could not be loaded!"<< endl;
+        cout << "Error: " << result.description() << endl;
+        return false;
+    }
+    else
+    {
+        cout<<"inserted successfully into the Trie!"<<endl;
     }
 
-    ~Trie()
+    //reference to https://pugixml.org/docs/quickstart.html
+    pugi::xml_node feedNode = doc.child("feed");
+    if(feedNode)
     {
-        deleteTrie(root);
-    }
-
-    void deleteTrie(TrieNode* node)
-    {
-        for(auto children:node->children)
+        for(pugi::xml_node docNode : feedNode.children("doc"))
         {
-            deleteTrie(children.second);
-        }
-        delete node;
-    }
-
-    void insert(const string& word)
-    {
-        TrieNode* currentNode = root;
-        for(char character:word)
-        {
-            if(currentNode->children.find(character) == currentNode->children.end())
+            pugi::xml_node titleNode = docNode.child("title");
+            pugi::xml_node abstractNode = docNode.child("abstract");
+            if(titleNode && abstractNode)
             {
-                currentNode->children[character] = new TrieNode();
+                string text = abstractNode.text().as_string();
+                stringstream ss(text);
+                string word;
+
+                while(ss>>word)
+                {
+                    for(int i = 0; i<word.size();++i)
+                    {
+                        if(!isalpha(word[i]))
+                        {
+                            word.erase(i,1);
+                            --i;
+                        }
+                    }
+
+                    if(!word.empty())
+                    {
+                        trie.insert(word);
+                    }
+                }
             }
-            currentNode = currentNode -> children[character];
-        }
-        currentNode->end_of_word = true;
-    }
-
-    bool search(const string& word)
-    {
-        if(cache.find(word)!=cache.end())
-        {
-            return cache[word];
-        }
-
-        TrieNode* currentNode = root;
-        for(char character:word)
-        {
-            if(currentNode -> children.find(character) == currentNode->children.end()) {
-                cache[word] = false;
-                return false;
-            }
-            currentNode = currentNode->children[character];
-        }
-
-        cache[word] = currentNode->end_of_word;
-        return currentNode->end_of_word;
-    }
-
-    bool starts_with(const string& word)
-    {
-        TrieNode* currentNode = root;
-        for(char character : word)
-        {
-            if(currentNode -> children.find(character) == currentNode->children.end())
-            {
-                return false;
-            }
-            currentNode = currentNode->children[character];
         }
         return true;
     }
+}
 
-    void find_words_with_prefix(const string& word)
-    {
-        TrieNode* currentNode = root;
-        for(char character : word)
-        {
-            if(currentNode -> children.find(character) == currentNode ->children.end())
-            {
-                return;
-            }
-            currentNode = currentNode->children[character];
-        }
-
-        vector<string> words;
-        queue<pair<TrieNode*, string>>q;
-        q.push({currentNode,word});
-
-        while(!q.empty())
-        {
-            auto[node,current_word] = q.front();
-            q.pop();
-
-            if (node->end_of_word) {
-                words.push_back(current_word);
-            }
-
-            for (auto& pair : node->children) {
-                q.push({pair.second, current_word + pair.first});
-            }
-        }
-
-        for (const string& word : words) {
-            cout << word << endl;
-        }
-
-    }
-
-};
-
-void parseWikipediaDumpIntoTrie(const string& filename, Trie& trie)
+bool parseWikipediaDumpIntoHashMap(const string& filename,hashTable& hashtable)
 {
     //reference to pugixml library
     pugi::xml_document doc;
@@ -140,7 +72,11 @@ void parseWikipediaDumpIntoTrie(const string& filename, Trie& trie)
     {
         cout << "XML file could not be loaded!"<< endl;
         cout << "Error: " << result.description() << endl;
-        return;
+        return false;
+    }
+    else
+    {
+        cout << "inserted successfully into the Hash Table!" << endl;
     }
 
     //reference to https://pugixml.org/docs/quickstart.html
@@ -170,119 +106,83 @@ void parseWikipediaDumpIntoTrie(const string& filename, Trie& trie)
 
                     if(!word.empty())
                     {
-                        trie.insert(word);
+                        hashtable.insert_word(word);
                     }
                 }
             }
         }
+        return true;
     }
 }
 
-void parseWikipediaDumpIntoHashMap(const string& filename,)
-{
-    //reference to pugixml library
-    pugi::xml_document doc;
-    pugi::xml_parse_result result= doc.load_file(filename.c_str());
-    if (!result)
-    {
-        cout << "XML file could not be loaded!"<< endl;
-        cout << "Error: " << result.description() << endl;
-        return;
-    }
-
-    //reference to https://pugixml.org/docs/quickstart.html
-    pugi::xml_node feedNode = doc.child("feed");
-    if(feedNode)
-    {
-        for(pugi::xml_node docNode : feedNode.children("doc"))
-        {
-            pugi::xml_node titleNode = docNode.child("title");
-            pugi::xml_node abstractNode = docNode.child("abstract");
-            if(titleNode && abstractNode)
-            {
-                string text = abstractNode.text().as_string();
-                stringstream ss(text);
-                string word;
-
-                while(ss>>word)
-                {
-                    for(int i = 0; i<word.size();++i)
-                    {
-                        if(!isalpha(word[i]))
-                        {
-                            word.erase(i,1);
-                            --i;
-                        }
-                    }
-
-                    if(!word.empty())
-                    {
-                        trie.insert(word);
-                    }
-                }
-            }
-        }
-    }
-}
-
-
-class HashTableImplementation{
-    unordered_map<string, bool> words_map;
-public:
-    //add the word into the hashtable and mark it true
-    //true means the word exists in the table
-    void insert_word(string word){
-        words_map[word] = true;
-    }
-
-    //if the word exist, return 1 since the key is unique in unordered map
-    // if doesnet then return 0
-    bool search(string word){
-        return words_map.count(word);
-    }
-
-
-    vector<string>same_prefix(string prefix){
-        vector<string> words_contain_prefix;
-        for(auto pair: words_map){
-            //if the key starts with the prefix, add it to vector
-            if(pair.first.find(prefix) == 0){
-                words_contain_prefix.push_back(pair.first);
-            }
-        }
-        return words_contain_prefix;
-    }
-};
 int main() {
+    hashTable hashTable;
     Trie trie;
-    HashTableImplementation hashTable;
+
     int option;
-    string input;
 
     cout << "Welcome to the Hash Table and Trie Search Engine!" << endl;
     bool control = true;
 
+
+        cout << "Insert a XML document into both the Trie and Hash Table and compare the insert time" << endl;
+        string input;
+        cout <<"Enter the path of the XML document you want to insert: ";
+        cin >> input;
+
+        auto start_trie_insert = chrono::high_resolution_clock::now();
+        bool isParseTrie = parseWikipediaDumpIntoTrie(input,trie);
+        auto end_trie_insert = chrono::high_resolution_clock::now();
+        auto duration_trie = chrono::duration_cast<chrono::nanoseconds>(end_trie_insert - start_trie_insert);
+
+        auto start_hash_insert = chrono::high_resolution_clock::now();
+        bool isParseHashmap = parseWikipediaDumpIntoHashMap(input,hashTable);
+        auto end_hash_insert = chrono::high_resolution_clock::now();
+        auto duration_hash = chrono::duration_cast<chrono::nanoseconds>(end_hash_insert - start_hash_insert);
+
+        if (isParseTrie)
+        {
+            cout << "XML insert to Trie: " << "in " << duration_trie.count() << " nanoseconds." << endl;
+        }
+        else
+        {
+            exit(0);
+        }
+
+        if (isParseHashmap)
+        {
+            cout << "XML insert to HashTable: " << "in " << duration_hash.count() << " nanoseconds." << endl;
+        }
+        else
+        {
+            exit(0);
+        }
+
+        if (isParseHashmap&&isParseTrie) {
+            if(duration_trie.count()>duration_hash.count())
+            {
+                double speedup = static_cast<double>(duration_trie.count()) / duration_hash.count();
+                cout << "Speedup insert from HashTable to Trie(Trie / HashTable): " << speedup << "x" << endl;
+            }
+            if(duration_trie.count()<duration_hash.count())
+            {
+                double speedup = static_cast<double>(duration_hash.count()) / duration_trie.count();
+                cout << "Speedup insert from Trie to HashTable(Hash Table / Trie): " << speedup << "x" << endl;
+            }
+        }
+
     while (control) {
         cout << "\nChoose an option:" << endl;
-        cout << "1. Insert a word into both the Trie and Hash Table" << endl;
-        cout << "2. Search for a word in both the Trie and Hash Table" << endl;
-        cout << "3. Find all words with a prefix in the Hash Table" << endl;
-        cout << "4. Compare search times for Trie and Hash Table" << endl;
-        cout << "5. Exit" << endl;
+        cout << "1. Search for a word in both the Trie and Hash Table and compare the exact search time" << endl;
+        cout << "2. Find all words with a prefix in both the Trie and Hash Table and compare the prefix search time" << endl;
+        cout << "3. Exit" << endl;
         cout << "Enter your option: ";
         cin >> option;
-
-        if (option == 1) {
-            cout << "Enter the word you want to insert: ";
+           if (option == 1) {
+            string input;
+            cout << "Enter the word you want to search for:";
             cin >> input;
-            trie.insert(input);
-            hashTable.insert_word(input);
-            cout << input << " inserted successfully into both the Trie and Hash Table!" << endl;
-
-        } else if (option == 2) {
-            cout << "Enter the word you want to search for: ";
-            cin >> input;
-
+            cout<<input<<endl;
 
             auto start_trie = chrono::high_resolution_clock::now();
             bool trie_result = trie.search(input);
@@ -297,55 +197,107 @@ int main() {
 
 
             cout << "\nSearch Results:" << endl;
-            cout << "Trie: " << (trie_result ? "Found" : "Not Found") << " in " << duration_trie.count() << " nanoseconds." << endl;
-            cout << "Hash Table: " << (hash_result ? "Found" : "Not Found") << " in " << duration_hash.count() << " nanoseconds." << endl;
 
-        } else if (option == 3) {
+            if (trie_result)
+            {
+                cout << "Trie: " << "Found "<< "in " << duration_trie.count() << " nanoseconds." << endl;
+            }
+            else
+            {
+                cout << "Trie: " << "Not Found "<< endl;
+            }
+
+            if (hash_result)
+            {
+                cout << "HashTable: " << "Found "<< "in " << duration_trie.count() << " nanoseconds." << endl;
+            }
+            else
+            {
+                cout << "HashTable: " << "Not Found "<< endl;
+            }
+
+            if(hash_result&&trie_result) {
+                if (duration_trie.count() > 0) {
+                    if (duration_trie.count() < duration_hash.count()) {
+                        double speedup = static_cast<double>(duration_hash.count()) / duration_trie.count();
+                        cout << "Speedup Exact Search from Trie to HashTable(Hash Table / Trie): " << speedup << "x"
+                             << endl;
+                    }
+                } else if (duration_hash.count() > 0) {
+                    if (duration_trie.count() > duration_hash.count()) {
+                        double speedup = static_cast<double>(duration_trie.count()) / duration_hash.count();
+                        cout << "Speedup Exact Search from HashTable to Trie(Trie / HashTable): " << speedup << "x"
+                             << endl;
+                    }
+                } else {
+
+                    cout << "Trie search time is too fast to measure speedup accurately." << endl;
+                }
+            }
+
+        } else if (option == 2) {
+            string input;
             cout << "Enter the prefix of the words you want to get: ";
             cin >> input;
             cout << "Words starting with: " << input << endl;
+
+            auto start_trie = chrono::high_resolution_clock::now();
+            trie.find_words_with_prefix(input);
+            auto end_trie = chrono::high_resolution_clock::now();
+            auto duration_trie = chrono::duration_cast<chrono::nanoseconds>(end_trie - start_trie);
+
+            auto start_hash = chrono::high_resolution_clock::now();
             vector<string> results = hashTable.same_prefix(input);
             if (results.empty()) {
                 cout << "No words found with the prefix '" << input << "'." << endl;
             } else {
+                cout<<"Hash Table Prefix Words Search:"<<endl;
+                int count=0;
                 for (const string& word : results) {
-                    cout << word << endl;
+                    cout << word<<" ";
+                    count++;
                 }
+                cout<<endl;
+                cout<<"Word Count: "<<count<<endl;
+                cout<<endl;
             }
-
-        } else if (option == 4) {
-            cout << "Enter the word to compare search times: ";
-            cin >> input;
-
-            auto start_trie = chrono::high_resolution_clock::now();
-            bool trie_result = trie.search(input);
-            auto end_trie = chrono::high_resolution_clock::now();
-            auto duration_trie = chrono::duration_cast<chrono::nanoseconds>(end_trie - start_trie);
-
-
-            auto start_hash = chrono::high_resolution_clock::now();
-            bool hash_result = hashTable.search(input);
             auto end_hash = chrono::high_resolution_clock::now();
             auto duration_hash = chrono::duration_cast<chrono::nanoseconds>(end_hash - start_hash);
 
-
-            cout << "\nPerformance Comparison:" << endl;
-            cout << "Trie: " << (trie_result ? "Found" : "Not Found") << " in " << duration_trie.count() << " nanoseconds." << endl;
-            cout << "Hash Table: " << (hash_result ? "Found" : "Not Found") << " in " << duration_hash.count() << " nanoseconds." << endl;
-
-
-            if (duration_trie.count() > 0) {
-                double speedup = static_cast<double>(duration_hash.count()) / duration_trie.count();
-                cout << "Speedup (Hash Table / Trie): " << speedup << "x" << endl;
-            } else {
-                cout << "Trie search time is too fast to measure speedup accurately." << endl;
+            if(!results.empty())
+            {
+                cout << "HashTable Searching all the words in " <<duration_hash.count() << " nanoseconds." << endl;
+                cout << "Trie Searching all the words in " <<duration_trie.count() << " nanoseconds." << endl;
             }
 
-        } else if (option == 5) {
-            control = false;
 
+               if (duration_trie.count() > 0) {
+                   if(duration_trie.count()<duration_hash.count())
+                   {
+                       double speedup = static_cast<double>(duration_hash.count()) / duration_trie.count();
+                       cout << "Speedup Exact Search from Trie to HashTable(Hash Table / Trie): " << speedup << "x" << endl;
+                   }
+               }
+               else if(duration_hash.count() > 0)
+               {
+                   if(duration_trie.count()>duration_hash.count())
+                   {
+                       double speedup = static_cast<double>(duration_trie.count()) / duration_hash.count();
+                       cout << "Speedup Exact Search from HashTable to Trie(Trie / HashTable): " << speedup << "x" << endl;
+                   }
+               }
+               else
+               {
+                   cout << "Trie search time is too fast to measure speedup accurately or there is no result" << endl;
+               }
+
+
+
+        } else if (option == 3) {
+            control = false;
         } else {
             cout << "Invalid option, please provide a valid instruction." << endl;
+            control=false;
         }
     }
 
